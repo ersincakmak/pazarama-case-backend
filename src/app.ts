2dc48f96c0b99b679a8bdf98cdab387776178fd0
ersin cakmak <ckmrsn@gmail.com>
2022-01-19 11:31:42 +0300

    Express configuration maded and dotenv and helmet packages added

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,16 @@
 import express from 'express'
+import helmet from 'helmet'
+import config from './config'
 
 const app = express()
 
+config()
+
+app.use(express.json())
+app.use(helmet())
+
 const PORT = process.env.PORT || 4000
 app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
   console.log(`Server is running on port:${PORT}`)
 })
