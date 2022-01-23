abe5871c61296f60c0848c5f98c0b0fce99f7ca8
ersin cakmak <ckmrsn@gmail.com>
2022-01-23 23:13:54 +0300

    Update allow headers

--- src/app.ts
+++ src/app.ts
@@ -30,7 +30,10 @@ app.use(
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 app.use(
   cors({
-    origin: ['http://localhost:3000'],
+    origin: [
+      'http://localhost:3000',
+      'https://pazarama-case-ersincakmak.netlify.app/',
+    ],
   })
 )
 
